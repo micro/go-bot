e10ca5ca620355e280437bb8c6fc7db1ba22de41
Asim Aslam <asim@aslam.me>
2019-05-21 08:22:02 +0000

    add discord and telegram by default

--- input/slack/slack.go
+++ input/slack/slack.go
@@ -27,13 +27,13 @@ func init() {
 func (p *slackInput) Flags() []cli.Flag {
 	return []cli.Flag{
 		cli.BoolFlag{
-			Name:  "slack_debug",
-			Usage: "Slack debug output",
+			Name:   "slack_debug",
+			Usage:  "Slack debug output",
 			EnvVar: "MICRO_SLACK_DEBUG",
 		},
 		cli.StringFlag{
-			Name:  "slack_token",
-			Usage: "Slack token",
+			Name:   "slack_token",
+			Usage:  "Slack token",
 			EnvVar: "MICRO_SLACK_TOKEN",
 		},
 	}
